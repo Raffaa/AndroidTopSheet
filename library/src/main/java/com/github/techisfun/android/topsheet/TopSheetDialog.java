ee2f8b83a57d690396997a16b9b06b852062e008
Hikaru Wada <watyaa@gmail.com>
2018-09-13 15:11:06 +0900

    default TopSheetDialog

--- library/src/main/java/com/github/techisfun/android/topsheet/TopSheetDialog.java
+++ library/src/main/java/com/github/techisfun/android/topsheet/TopSheetDialog.java
@@ -16,6 +16,7 @@ package com.github.techisfun.android.topsheet;
  */
 
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.LayoutRes;
@@ -23,11 +24,16 @@ import android.support.annotation.NonNull;
 import android.support.annotation.StyleRes;
 import android.support.design.widget.BottomSheetBehavior;
 import android.support.design.widget.CoordinatorLayout;
+import android.support.v4.view.AccessibilityDelegateCompat;
+import android.support.v4.view.ViewCompat;
+import android.support.v4.view.accessibility.AccessibilityNodeInfoCompat;
 import android.support.v7.app.AppCompatDialog;
 import android.util.TypedValue;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.Window;
+import android.view.WindowManager;
 import android.widget.FrameLayout;
 
 /**
@@ -37,9 +43,12 @@ public class TopSheetDialog extends AppCompatDialog {
 
     private TopSheetBehavior<FrameLayout> topSheetBehavior;
 
+    boolean mCancelable = true;
+    private boolean mCanceledOnTouchOutside = true;
+    private boolean mCanceledOnTouchOutsideSet;
+
     public TopSheetDialog(@NonNull Context context) {
-        super(context, getThemeResId(context, 0));
-        supportRequestWindowFeature(Window.FEATURE_NO_TITLE);
+        this(context, 0);
     }
 
     public TopSheetDialog(@NonNull Context context, @StyleRes int theme) {
@@ -53,6 +62,7 @@ public class TopSheetDialog extends AppCompatDialog {
                              OnCancelListener cancelListener) {
         super(context, cancelable, cancelListener);
         supportRequestWindowFeature(Window.FEATURE_NO_TITLE);
+        mCancelable = cancelable;
     }
 
     @Override
@@ -63,8 +73,15 @@ public class TopSheetDialog extends AppCompatDialog {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        getWindow().setLayout(
-                ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
+        Window window = getWindow();
+        if (window != null) {
+            if (Build.VERSION.SDK_INT >= 21) {
+                window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+                window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            }
+            window.setLayout(ViewGroup.LayoutParams.MATCH_PARENT,
+                    ViewGroup.LayoutParams.MATCH_PARENT);
+        }
     }
 
     @Override
@@ -77,56 +94,105 @@ public class TopSheetDialog extends AppCompatDialog {
         super.setContentView(wrapInTopSheet(0, view, params));
     }
 
+    @Override
+    public void setCancelable(boolean cancelable) {
+        super.setCancelable(cancelable);
+        if (mCancelable != cancelable) {
+            mCancelable = cancelable;
+            if (topSheetBehavior != null) {
+                topSheetBehavior.setHideable(cancelable);
+            }
+        }
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if (topSheetBehavior != null) {
+            topSheetBehavior.setState(TopSheetBehavior.STATE_COLLAPSED);
+        }
+    }
+
+    @Override
+    public void setCanceledOnTouchOutside(boolean cancel) {
+        super.setCanceledOnTouchOutside(cancel);
+        if (cancel && !mCancelable) {
+            mCancelable = true;
+        }
+        mCanceledOnTouchOutside = cancel;
+        mCanceledOnTouchOutsideSet = true;
+    }
+
     private View wrapInTopSheet(int layoutResId, View view, ViewGroup.LayoutParams params) {
-        final CoordinatorLayout coordinator = (CoordinatorLayout) View.inflate(getContext(),
-                R.layout.top_sheet_dialog, null);
+        final FrameLayout container = (FrameLayout) View.inflate(getContext(), R.layout.top_sheet_dialog, null);
+        final CoordinatorLayout coordinator = (CoordinatorLayout) container.findViewById(R.id.coordinator);
         if (layoutResId != 0 && view == null) {
             view = getLayoutInflater().inflate(layoutResId, coordinator, false);
         }
         FrameLayout topSheet = (FrameLayout) coordinator.findViewById(R.id.design_top_sheet);
         topSheetBehavior = TopSheetBehavior.from(topSheet);
         topSheetBehavior.setTopSheetCallback(mTopSheetCallback);
+        topSheetBehavior.setHideable(mCancelable);
         if (params == null) {
             topSheet.addView(view);
         } else {
             topSheet.addView(view, params);
         }
         // We treat the CoordinatorLayout as outside the dialog though it is technically inside
-        if (shouldWindowCloseOnTouchOutside()) {
-            coordinator.findViewById(R.id.top_sheet_touch_outside).setOnClickListener(
-                    new View.OnClickListener() {
-                        @Override
-                        public void onClick(View view) {
-                            if (isShowing()) {
-                                cancel();
-                            }
-                        }
-                    });
-        }
-        return coordinator;
+        coordinator.findViewById(R.id.top_sheet_touch_outside).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (mCancelable && isShowing() && shouldWindowCloseOnTouchOutside()) {
+                    cancel();
+                }
+            }
+        });
+        // Handle accessibility events
+        ViewCompat.setAccessibilityDelegate(topSheet, new AccessibilityDelegateCompat() {
+            @Override
+            public void onInitializeAccessibilityNodeInfo(View host,
+                                                          AccessibilityNodeInfoCompat info) {
+                super.onInitializeAccessibilityNodeInfo(host, info);
+                if (mCancelable) {
+                    info.addAction(AccessibilityNodeInfoCompat.ACTION_DISMISS);
+                    info.setDismissable(true);
+                } else {
+                    info.setDismissable(false);
+                }
+            }
+
+            @Override
+            public boolean performAccessibilityAction(View host, int action, Bundle args) {
+                if (action == AccessibilityNodeInfoCompat.ACTION_DISMISS && mCancelable) {
+                    cancel();
+                    return true;
+                }
+                return super.performAccessibilityAction(host, action, args);
+            }
+        });
+        topSheet.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent event) {
+                // Consume the event and prevent it from falling through
+                return true;
+            }
+        });
+        return container;
     }
 
-    private boolean shouldWindowCloseOnTouchOutside() {
-        if (true) {
-            return true;
+    boolean shouldWindowCloseOnTouchOutside() {
+        if (!mCanceledOnTouchOutsideSet) {
+            if (Build.VERSION.SDK_INT < 11) {
+                mCanceledOnTouchOutside = true;
+            } else {
+                TypedArray a = getContext().obtainStyledAttributes(
+                        new int[]{android.R.attr.windowCloseOnTouchOutside});
+                mCanceledOnTouchOutside = a.getBoolean(0, true);
+                a.recycle();
+            }
+            mCanceledOnTouchOutsideSet = true;
         }
-
-        if (Build.VERSION.SDK_INT < 11) {
-            return true;
-        }
-        TypedValue value = new TypedValue();
-        //noinspection SimplifiableIfStatement
-        if (getContext().getTheme()
-                .resolveAttribute(android.R.attr.windowCloseOnTouchOutside, value, true)) {
-            return value.data != 0;
-        }
-        return false;
-    }
-
-    @Override
-    public void show() {
-        super.show();
-        //topSheetBehavior.setState(TopSheetBehavior.STATE_EXPANDED);
+        return mCanceledOnTouchOutside;
     }
 
     private static int getThemeResId(Context context, int themeId) {
